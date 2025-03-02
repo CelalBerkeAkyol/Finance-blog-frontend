0689e4adcb812b1adfc35b36be4335aa53bcdca1
CelalBerkeAkyol <buscberke@gmail.com>
2025-03-02 21:19:08 +0300

    Hide login and register buttons for authenticated users

--- src/components/header/CustomNavbar.jsx
+++ src/components/header/CustomNavbar.jsx
@@ -12,20 +12,7 @@ import {
   DropdownItem,
 } from "@nextui-org/react";
 import SearchModal from "../yardımcılar/SearchModal";
-
-// Logo Bileşeni
-export const AcmeLogo = () => {
-  return (
-    <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
-      <path
-        clipRule="evenodd"
-        d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-        fill="currentColor"
-        fillRule="evenodd"
-      />
-    </svg>
-  );
-};
+import { useSelector } from "react-redux";
 
 export default function CustomNavbar() {
   const [isSearchOpen, setIsSearchOpen] = useState(false);
@@ -45,16 +32,15 @@ export default function CustomNavbar() {
     { name: "Finans", path: "/blog/category/finans" },
     { name: "Kişisel Finans", path: "/blog/category/kişisel-finans" },
   ];
-
+  const { isLoggedIn } = useSelector((state) => state.user);
   return (
     <>
       <Navbar className="bg-gray-50 w-screen" maxWidth="xl">
-        <NavbarContent className="flex" justify="start">
-          <AcmeLogo />
+        <NavbarContent className="flex" justify="center">
           <p className="font-bold text-inherit text-lg">Fin AI</p>
         </NavbarContent>
 
-        <NavbarContent className="hidden sm:flex gap-2" justify="start">
+        <NavbarContent className="hidden sm:flex " justify="center">
           {navbarLinks.map((item, index) => (
             <NavbarItem key={index}>
               <button
@@ -89,39 +75,42 @@ export default function CustomNavbar() {
         </NavbarContent>
 
         {/* Sağ Tarafa Eklenen Arama ve Kullanıcı Butonları */}
-        <NavbarContent justify="end" className="gap-4">
+        <NavbarContent justify="center">
           <NavbarItem>
             <Button
               variant="bordered"
               color="secondary"
               radius="lg"
-              startContent={<Icon icon="material-symbols:search" width="16" />}
+              startContent={<Icon icon="material-symbols:search" width="14" />}
               size="md"
               onClick={() => setIsSearchOpen(true)}
             >
               Ara
             </Button>
           </NavbarItem>
-
           {/* Login Icon */}
-          <NavbarItem>
-            <button
-              onClick={() => navigate("/login")}
-              className=" pl-4 hover:text-primary"
-            >
-              Login
-            </button>
-          </NavbarItem>
 
-          {/* Register Icon */}
-          <NavbarItem>
-            <button
-              onClick={() => navigate("/register")}
-              className=" hover:text-primary"
-            >
-              Register
-            </button>
-          </NavbarItem>
+          {!isLoggedIn && (
+            <>
+              <NavbarItem>
+                <button
+                  onClick={() => navigate("/login")}
+                  className="pl-4 hover:text-primary"
+                >
+                  Login
+                </button>
+              </NavbarItem>
+
+              <NavbarItem>
+                <button
+                  onClick={() => navigate("/register")}
+                  className="hover:text-primary"
+                >
+                  Register
+                </button>
+              </NavbarItem>
+            </>
+          )}
         </NavbarContent>
       </Navbar>
 

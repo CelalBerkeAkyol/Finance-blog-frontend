67c0f27e057a26f147e916b4095241c02762d22f
CelalBerkeAkyol <buscberke@gmail.com>
2025-03-11 21:38:22 +0300

    Reduced user component codes

---
+++ src/components/modals/DeleteUserModal.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+} from "@nextui-org/react";
+
+const DeleteUserModal = ({
+  isOpen,
+  onClose,
+  selectedUser,
+  deleteError,
+  handleDeleteUser,
+  isLoading,
+}) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalContent>
+        {(onCloseModal) => (
+          <>
+            <ModalHeader className="flex flex-col gap-1">
+              Kullanıcı Silme Onayı
+            </ModalHeader>
+            <ModalBody>
+              {selectedUser && (
+                <p>
+                  <b>{selectedUser.userName}</b> ({selectedUser.email})
+                  kullanıcısını silmek istediğinize emin misiniz? Bu işlem geri
+                  alınamaz.
+                </p>
+              )}
+              {deleteError && <p className="text-danger">{deleteError}</p>}
+            </ModalBody>
+            <ModalFooter>
+              <Button variant="flat" onPress={onCloseModal}>
+                İptal
+              </Button>
+              <Button
+                color="danger"
+                onPress={handleDeleteUser}
+                isLoading={isLoading}
+              >
+                Sil
+              </Button>
+            </ModalFooter>
+          </>
+        )}
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default DeleteUserModal;

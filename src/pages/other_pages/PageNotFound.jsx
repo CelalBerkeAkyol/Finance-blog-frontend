45348615296fe688c4ad3e8a74caf2b901160a8c
CelalBerkeAkyol <buscberke@gmail.com>
2025-03-18 11:15:13 +0300

    error component replaced

--- src/pages/other_pages/PageNotFound.jsx
+++ src/pages/other_pages/PageNotFound.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import NotFoundErrorComponent from "../../components/uyarılar/NotFoundErrorComponent";
 import BannerComponent from "../../components/header/BannerComponent";
 import CustomNavbar from "../../components/header/CustomNavbar";
+import ErrorComponent from "../../components/uyarılar/ErrorComponent";
 
 export default function PageNotFound() {
   return (
@@ -9,7 +9,7 @@ export default function PageNotFound() {
       <BannerComponent />
       <CustomNavbar />
       <div className="flex-grow flex items-center justify-center py-12">
-        <NotFoundErrorComponent message="Üzgünüz, aradığınız sayfa bulunamadı." />
+        <ErrorComponent message="Üzgünüz, aradığınız sayfa bulunamadı." />
       </div>
     </div>
   );
